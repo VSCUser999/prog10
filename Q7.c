552f93642351bd9e219a34dd81fbeb318a3e6ab7
VSCUser999 <def8663@gmail.com>
2021-10-20 14:07:45 +0800

    HM20211020_1407

---
+++ Q7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void){
+    int n, k = 1, j = 2;
+    scanf("%d",&n);
+
+    while(k != 0){
+
+        if(n == 1){
+           printf("NO\n");
+            return 0;
+        }
+        k = n % j;
+        j++;
+
+       if(j >= n) 
+        break;   
+    }
+
+    if(k == 0)
+        printf("NO\n");
+    else
+        printf("YES\n");
+}
